e61822972ff57fc198cf0506db3c14a2ad693b24
Johannes <johnnyt@gmx.at>
2020-07-12 11:48:10 +0200

    IE5 Browser Compatibility

--- searchbar-experiment/js/search.js
+++ searchbar-experiment/js/search.js
@@ -19,6 +19,11 @@ function updateSearch(searchText) {
     return;
   }
   showResults();
+  getSearchResults(searchText);
+}
+
+function getSearchResultsWithFetch(searchText) {
+  var matchlist = document.getElementById("searchmatches");
   var regex = new RegExp("^" + searchText, "gi");
   fetch("../data/state_capitals.json")
     .then(function (response) {
@@ -36,25 +41,80 @@ function updateSearch(searchText) {
     });
 }
 
+function getSearchResults(searchText) {
+  httpGetJson("../data/state_capitals.json", getHttpRequest(), function (jsonResult) {
+    displayResults(filterResults(jsonResult, searchText));
+  });
+}
+
+function filterResults(jsonResults, searchText) {
+  var regex = new RegExp("^" + searchText, "gi");
+  return jsonResults.filter(function (entry) {
+    return entry.name.match(regex) || entry.abbr.match(regex);
+  });
+}
+
+function displayResults(jsonResults) {
+  var matchlist = document.getElementById("searchmatches");
+  jsonResults.forEach(function (entry) {
+    matchlist.innerHTML += "<li>" + entry.name + " (" + entry.abbr + ")" + "</li> ";
+  });
+}
+
 function showResults() {
-  hideResults();
   var element = document.getElementById("searchresult");
-  var name = "show";
-  element.className += " " + name;
+  addClass("show", element);
 }
 
 function hideResults() {
   var element = document.getElementById("searchresult");
-  element.className = element.className.replace(/\bshow\b/g, "");
+  removeClass("show", element);
 }
 
-function addEvent(evnt, elem, func) {
-  if (elem.addEventListener)  // W3C DOM
-     elem.addEventListener(evnt,func,false);
-  else if (elem.attachEvent) { // IE DOM
-     elem.attachEvent("on"+evnt, func);
+function addClass(classToAdd, element) {
+  removeClass(classToAdd, element);
+  element.className += " " + classToAdd;
+}
+
+function removeClass(classToRemove, element) {
+  var regex = new RegExp("\\s?\\b" + classToRemove + "\\b", "gi");
+  element.className = element.className.replace(regex, "");
+}
+
+function addEvent(eventName, element, eventHandler) {
+  if (element.addEventListener)
+    element.addEventListener(eventName, eventHandler, false);
+  else if (element.attachEvent) {
+    element.attachEvent("on" + eventName, eventHandler);
+  } else {
+    element["on" + eventName] = eventHandler;
   }
-  else { // No much to do
-     elem["on"+evnt] = func;
+}
+
+function httpGetJson(url, httpRequest, onJsonResultReceived) {
+  httpRequest.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var jsonResult = JSON.parse(this.responseText);
+      onJsonResultReceived(jsonResult);
+    }
+  };
+  httpRequest.open("GET", url, true);
+  httpRequest.send();
+}
+
+function getHttpRequest() {
+  if (typeof XMLHttpRequest !== "undefined") {
+    return new XMLHttpRequest();
   }
+  try {
+    return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+  } catch (e) {}
+  try {
+    return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+  } catch (e) {}
+  try {
+    return new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (e) {}
+  // Microsoft.XMLHTTP points to Msxml2.XMLHTTP and is redundant
+  throw new Error("This browser does not support XMLHttpRequest.");
 }
