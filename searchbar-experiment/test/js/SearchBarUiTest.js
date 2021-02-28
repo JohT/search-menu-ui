a99d3dab98f3762a53d8e4a6e5d768761475882b
Johannes <johnnyt@gmx.at>
2021-02-28 12:23:33 +0100

    refactored document element tests

--- searchbar-experiment/test/js/SearchBarUiTest.js
+++ searchbar-experiment/test/js/SearchBarUiTest.js
@@ -12,58 +12,99 @@ describe("search.js SearchBarUI", function () {
     searchUnderTest = searchbar || require("../../src/js/search.js");
   });
 
+  /**
+   * This callback will be called on every document element that is discovered the first time.
+   * @callback NewlyDiscoveredElement
+   * @param {Element} element 
+   */
+
+  /**
+   * Collects all elements that are queried by "document.getElementById".
+   * @param {Elements} documentElements 
+   * @param {NewlyDiscoveredElement} onNewlyDiscoveredElement 
+   */
+  function collectElementsById(documentElements, onNewlyDiscoveredElement) {
+    document.getElementById = jasmine.createSpy("HTML Element").and.callFake(function (id) {
+      if (!documentElements[id]) {
+        var newElement = document.createElement("div");
+        newElement.id = id;
+        documentElements[id] = newElement;
+        onNewlyDiscoveredElement(newElement);
+      }
+      return documentElements[id];
+    });
+  }
+
+  /**
+   * Collects all event listeners that are registered with "element.addEventListener".
+   * The event listeners can then be accessed by "eventListeners.<id of the element>.<name of the event>".
+   * @param {Object} eventListeners - whenever a listener is registered with "element.addEventListener", it will be collected in this object.
+   * @param {Element} element 
+   */
+  function collectEventListeners(eventListeners, element) {
+    var id = element.id;
+    element.addEventListener = jasmine.createSpy("HTML EventListener of " + id).and.callFake(function (eventName, eventHandler) {
+      if (!eventListeners[id]) {
+        eventListeners[id] = {};
+      }
+      if (!eventListeners[id][eventName]) {
+        eventListeners[id][eventName] = eventHandler;
+      } else {
+        var alreadyExistingHandler = eventListeners[id][eventName];
+        eventListeners[id][eventName] = function (event) {
+          alreadyExistingHandler(event);
+          eventHandler(event);
+        };
+      }
+    });
+  }
+
+  /**
+   * Creates a dummy key event for event handler tests.
+   * @param {String} keyName 
+   * @param {Element} targetElement 
+   */
+  function createKeyEvent(keyName, targetElement) {
+    return {
+      key: keyName,
+      currentTarget: targetElement
+    };
+  }
+
   describe("SearchBarUI", function () {
     var searchBarUiUnderTest;
     var config;
     var searchService = jasmine.createSpy("searchServiceSpy");
     var dataConverter = jasmine.createSpy("dataConverterSpy");
     var documentElements = {};
-    var eventHandlers = {};
-    //id "searchbar"
+    var eventListeners = {};
 
     beforeEach(function () {
-      document.getElementById = jasmine.createSpy("HTML Element").and.callFake(function (id) {
-        if (documentElements[id]) {
-          return documentElements[id];
-        }
-        var newElement = document.createElement("div");
-        documentElements[id] = newElement;
-        newElement.addEventListener = jasmine.createSpy("HTML EventListener of " + id).and.callFake(function (eventName, eventHandler) {
-          if (!eventHandlers[id]) {
-            eventHandlers[id] = {};
-          }
-          if (!eventHandlers[id][eventName]) {
-            eventHandlers[id][eventName] = eventHandler;
-          } else {
-            var alreadyExistingHandler = eventHandlers[id][eventName];
-            eventHandlers[id][eventName] = function (event) {
-              alreadyExistingHandler(event);
-              eventHandler(event);
-            };
-          }
-        });
-
-        return documentElements[id];
+      collectElementsById(documentElements, function (newElement) {
+        collectEventListeners(eventListeners, newElement);
       });
       searchBarUiUnderTest = new searchUnderTest.SearchbarAPI().searchService(searchService).dataConverter(dataConverter).start();
       config = searchBarUiUnderTest.config;
     });
 
-    it("should add key down event listeners to the input element with default id `searchbar`", function () {
+    function getResultViewElement() {
+      return documentElements[config.resultsView.viewElementId];
+    }
+
+    it("should add key down event listeners to the input element", function () {
       expect(document.getElementById).toHaveBeenCalledWith("searchbar");
       expect(documentElements.searchbar.addEventListener).toHaveBeenCalledWith("keydown", jasmine.any(Function), false);
-      expect(eventHandlers.searchbar.keydown).toBeDefined();
+      expect(eventListeners.searchbar.keydown).toBeDefined();
     });
 
+    it("should set search input text to `` when escape key is pressed", function () {
+      eventListeners.searchbar.keydown(createKeyEvent("Escape", documentElements.searchbar)); 
+      expect(documentElements.searchbar.value).toEqual("");
+    });
 
     it("should hide search area when escape key is pressed", function () {
-      var escapeKeyEvent = {
-        key: "Escape",
-        currentTarget: documentElements.searchbar
-      };
-      eventHandlers.searchbar.keydown(escapeKeyEvent); // simulate pressed escape key
-      expect(documentElements.searchbar.value).toEqual("");
-      expect(documentElements[config.resultsView.viewElementId].className).not.toContain("show");
+      eventListeners.searchbar.keydown(createKeyEvent("Escape", documentElements.searchbar));
+      expect(getResultViewElement().className).not.toContain("show");
     });
   });
 });
