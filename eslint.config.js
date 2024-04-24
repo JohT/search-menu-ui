9d5e1dd47d4a7cae66c30a5cbbbf683316b17832
JohT <johnnyt@gmx.at>
2024-04-24 07:42:29 +0200

    Migrate to eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+let globals = require("globals");
+let js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
